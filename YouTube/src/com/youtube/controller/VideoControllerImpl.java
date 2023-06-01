9be9e7c4178783590d04a662f32baaabeacb2bb6
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-06-01 14:58:15 +0900

    z

--- YouTube/src/com/youtube/controller/VideoControllerImpl.java
+++ YouTube/src/com/youtube/controller/VideoControllerImpl.java
@@ -4,6 +4,14 @@ import com.youtube.model.Video;
 
 public interface VideoControllerImpl {
 	
+	/*
+	 * CRUD
+	 * Create : 추가
+	 * Read : 읽기 (1개, 목록)
+	 * Update : 수정
+	 * Delete : 삭제
+	 */
+	
 	public Video upload(); // 영상 업로드
 	public Video[] VideoList();// 영상 목록
 	public Video viewVideo();// 영상 1개 보기
