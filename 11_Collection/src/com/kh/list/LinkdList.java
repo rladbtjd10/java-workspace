6055ee97066e6acbefb0f74d10db047c68ab2065
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-06-05 15:21:43 +0900

    q

---
+++ 11_Collection/src/com/kh/list/LinkdList.java
@@ -0,0 +1,98 @@
+package com.kh.list;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class LinkdList implements Queue {
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return false;
+	}
+
+	@Override
+	public Iterator iterator() {
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		return null;
+	}
+
+	@Override
+	public Object[] toArray(Object[] a) {
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection c) {
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection c) {
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection c) {
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection c) {
+		return false;
+	}
+
+	@Override
+	public void clear() {
+	}
+
+	@Override
+	public boolean add(Object e) {
+		return false;
+	}
+
+	@Override
+	public boolean offer(Object e) {
+		return false;
+	}
+
+	@Override
+	public Object remove() {
+		return null;
+	}
+
+	@Override
+	public Object poll() {
+		return null;
+	}
+
+	@Override
+	public Object element() {
+		return null;
+	}
+
+	@Override
+	public Object peek() {
+		return null;
+	}
+
+}
