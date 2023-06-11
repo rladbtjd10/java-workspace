52f9edc0c4adac0340624dbd50134ca9b8891879
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-06-11 13:03:43 +0900

    z

--- 06_Object/src/com/kh/example/practice3/model/Lotto.java
+++ 06_Object/src/com/kh/example/practice3/model/Lotto.java
@@ -9,11 +9,11 @@ public class Lotto {
 	public String information() {
 		
 		for(int i=0; i<6; i++) {
-			lotto[i] = (int)(Math.random() * 45 + 1);
+			lotto[i] = (int)((Math.random() * 45) + 1);
 
 			//중복제거!!
 			for(int j=0; j<i; j++) {
-				if(lotto [i] == lotto[j]) {
+				if(lotto[i] == lotto[j]) {
 					i--;
 					break;
 				}
