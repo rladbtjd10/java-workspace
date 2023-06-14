870b262912d88a588a0a533fc1300bdac63ab188
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-06-14 23:24:02 +0900

    sas

--- Netflix/src/com/netflix/controller/VideoController.java
+++ Netflix/src/com/netflix/controller/VideoController.java
@@ -1,7 +1,12 @@
 package com.netflix.controller;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
+import com.netflix.model.User;
 import com.netflix.model.Video;
 
 public class VideoController {
@@ -15,30 +20,87 @@ public class VideoController {
 	 */
 	
 	ArrayList<Video> videoList = new ArrayList<>();
+	UserController userController = new UserController();
 
-	
+
 	public void upload(Video video) { //영상 업로드
 		videoList.add(video);
 	}
 
-	
+
 	public ArrayList<Video> VideoList() { //동영상 목록
 		return videoList;
 	}
 
-	
+
 	public Video viewVideo(int index) { // 동영상 1개 보기
 		return videoList.get(index);
 	}
 
-	
+
 	public Video updateVideo(int index, Video video) { //동영상 업데이트
 		return videoList.set(index, video);
 	}
 
-	
-	public boolean deleteVideo(Video video) { //동영상 삭제
-		return videoList.remove(video);
+
+	// 영상 검색하기
+	public Video[] searchVideo(String keyword) {
+		List<Video> searchResults = new ArrayList<>();
+		for (Video video : videoList) {
+			if (video.title.contains(keyword) || video.getDescription().contains(keyword)) {
+				searchResults.add(video);
+			}
+		}
+		return searchResults.toArray(new Video[0]);
+	}
+
+	// 영상 평가하기
+	public void rateVideo(Video video, int rating) {
+		video.setRating(rating);
+	}
+
+	// 영상 공유하기
+	public void shareVideo(Video video, String platform) {
+		// 공유 로직 구현
+		System.out.println("Sharing video on " + platform + ": " + video.getTitle());
+	}
+
+	// 인기 영상 목록 조회하기
+	public Video[] getPopularVideos() {
+		// 인기 영상 로직 구현
+		// 예시로서 랜덤하게 영상을 선택
+		Random random = new Random();
+		int numVideos = Math.min(videoList.size(), 5); // 최대 5개의 영상 반환
+		Set<Integer> selectedIndices = new HashSet<>();
+		List<Video> popularVideos = new ArrayList<>();
+		while (selectedIndices.size() < numVideos) {
+			int index = random.nextInt(videoList.size());
+			if (!selectedIndices.contains(index)) {
+				selectedIndices.add(index);
+				popularVideos.add(videoList.get(index));
+			}
+		}
+		return popularVideos.toArray(new Video[0]);
+	}
+
+	// 영상 재생 시간 계산하기
+	public int calculateVideoDuration(Video video) {
+		return video.getDuration();
+	}
+
+	// 사용자별 좋아요한 영상 목록 조회하기
+	public Video[] getLikedVideos(User user) {
+		List<Video> likedVideos = new ArrayList<>();
+		for (Video video : videoList) {
+			if (video.isLikedByUser(user)) {
+				likedVideos.add(video);
+			}
+		}
+		return likedVideos.toArray(new Video[0]);
 	}
 	
 }
+
+
+
+
--- Netflix/src/com/netflix/model/Video.java
+++ Netflix/src/com/netflix/model/Video.java
@@ -1,5 +1,6 @@
 package com.netflix.model;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale.Category;
@@ -8,7 +9,7 @@ import javax.xml.stream.events.Comment;
 
 public class Video {
 	
-	private String title;
+	public String title;
 	private Date uploadAt;
 	private int views; 
 	private String imgUrl;
@@ -18,6 +19,28 @@ public class Video {
 	
 	private List<Comment> comments;
 	private Category category;
+	public ArrayList<Video> getDescription() {
+		return null;
+	}
+	public void setRating(int rating) {
+		
+	}
+	public boolean isLikedByUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public int getDuration() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public boolean isLikedByUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 		
 		
 		
