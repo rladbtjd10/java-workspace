54940fd50fd061fc2f47374fc898b3d307a20ea0
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-06-16 14:24:43 +0900

    zz

--- 04_Control/src/com/kh/condition/A_If.java
+++ 04_Control/src/com/kh/condition/A_If.java
@@ -292,12 +292,12 @@ public class A_If {
 		A_If a  = new A_If();
 //		a.method1();
 //		a.method2();
-//		a.method3();
+		a.method3();
 //		a.method4();
 //		a.method5();
 //		a.method6();
 //		a.method7();
-		a.practice2();
+//		a.practice2();
 		
 		/*
 		 * if문
